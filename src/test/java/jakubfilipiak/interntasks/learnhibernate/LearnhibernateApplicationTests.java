f6169faf6ac5926f24f975de75f4bd6299d064d3
Jakub Filipiak <jakub.filipiak@billennium.com>
2019-11-07 15:41:01 +0100

    Init repo

---
+++ src/test/java/jakubfilipiak/interntasks/learnhibernate/LearnhibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package jakubfilipiak.interntasks.learnhibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnhibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
